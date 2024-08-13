1a8b68cd0609fc32dbcffe29838abc99be90bcd8
William McPherson <willmcpherson2@gmail.com>
2024-08-13 18:44:18 +1000

    Add eval module

---
+++ src/eval.rs
@@ -0,0 +1,5 @@
+use crate::ast::*;
+
+pub fn eval_program(program: Program) -> Result<Program, String> {
+    Ok(program)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
-use crate::parse::parse_program;
+use crate::{eval::eval_program, parse::parse_program};
+
+use std::{env, fs};
 
 mod ast;
+mod eval;
 mod parse;
 
 fn main() {
-    println!(
-        "{:#?}",
-        parse_program(
-            r#"
-/* welcome to
-my database */
+    let Some(filename) = env::args().nth(1) else {
+        eprintln!("Usage: drd <filename>");
+        return;
+    };
 
-Staff =
-  name: 'Alice'; -- first row
-  name: 'Bob'    -- second row
+    let input = match fs::read_to_string(filename) {
+        Ok(input) => input,
+        Err(e) => {
+            eprintln!("Error reading file: {}", e);
+            return;
+        }
+    };
 
-bob = name /* add more columns here */ <- Staff ? name == 'Bob'
-"#
-        )
-    );
+    let program = match parse_program(&input) {
+        Ok((_, program)) => program,
+        Err(e) => {
+            eprintln!("Error parsing program: {}", e);
+            return;
+        }
+    };
+
+    let program = match eval_program(program) {
+        Ok(program) => program,
+        Err(e) => {
+            eprintln!("Error evaluating program: {}", e);
+            return;
+        }
+    };
+
+    println!("{:#?}", program);
 }
