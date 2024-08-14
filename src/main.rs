1769faad0f68334adbee7c48ba3706b8d818ecab
William McPherson <willmcpherson2@gmail.com>
2024-08-14 21:14:47 +1000

    Use clap

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
-use crate::{eval::eval_exp, parse::parse_exp};
+use clap::Parser;
+use std::fs;
 
-use std::{env, fs};
+use crate::{eval::eval_exp, parse::parse_exp};
 
 mod ast;
 mod eval;
 mod parse;
 
+/// The Drd programming language
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Input file to process
+    #[arg(conflicts_with = "eval")]
+    file: Option<String>,
+
+    /// Evaluate a string instead of a file
+    #[arg(short, long, value_name = "STRING")]
+    eval: Option<String>,
+}
+
 fn main() {
-    let Some(filename) = env::args().nth(1) else {
-        eprintln!("Usage: drd <filename>");
+    let cli = Cli::parse();
+
+    let text = if let Some(text) = cli.eval {
+        text
+    } else if let Some(filename) = cli.file {
+        match fs::read_to_string(filename) {
+            Ok(text) => text,
+            Err(e) => {
+                eprintln!("Error reading file: {}", e);
+                return;
+            }
+        }
+    } else {
+        eprintln!("Either a file or --eval must be provided");
         return;
     };
 
-    let input = match fs::read_to_string(filename) {
-        Ok(input) => input,
-        Err(e) => {
-            eprintln!("Error reading file: {}", e);
-            return;
-        }
-    };
-
-    let program = match parse_exp(&input) {
+    let program = match parse_exp(&text) {
         Ok((_, program)) => program,
         Err(e) => {
             eprintln!("Error parsing program: {}", e);
