2bc2df9d97d64e5363be69ab8ed446a1454cd6f1
William McPherson <willmcpherson2@gmail.com>
2024-08-21 22:38:58 +1000

    Pass env by reference in eval

--- src/eval.rs
+++ src/eval.rs
@@ -5,48 +5,49 @@ use crate::exp::{Exp, Exp::*};
 type Env = HashMap<String, Exp>;
 
 pub fn eval(exp: Exp) -> Result<(Exp, Env), String> {
-    eval_exp(exp, HashMap::new())
+    eval_exp(exp, &HashMap::new())
 }
 
-fn eval_exp(exp: Exp, mut env: Env) -> Result<(Exp, Env), String> {
+fn eval_exp(exp: Exp, env: &Env) -> Result<(Exp, Env), String> {
     match exp {
         Let(var, exp, body) => {
-            let (exp, _) = eval_exp(*exp, env.clone())?;
+            let (exp, _) = eval_exp(*exp, env)?;
+            let mut env = env.clone();
             env.insert(var, exp);
-            eval_exp(*body, env)
+            eval_exp(*body, &env)
         }
         Select(select_vars, table) => {
-            let (Table(table_vars, exps), _) = eval_exp(*table, env.clone())? else {
+            let (Table(table_vars, exps), _) = eval_exp(*table, env)? else {
                 return Err("expected table".to_string());
             };
             let exps = select(&select_vars, &table_vars, exps);
-            Ok((Table(select_vars, exps), env))
+            Ok((Table(select_vars, exps), env.clone()))
         }
         Where(table, cond) => {
-            let (Table(table_vars, exps), _) = eval_exp(*table, env.clone())? else {
+            let (Table(table_vars, exps), _) = eval_exp(*table, env)? else {
                 return Err("expected table".to_string());
             };
-            let exps = filter(&table_vars, exps, *cond, env.clone())?;
-            Ok((Table(table_vars, exps), env))
+            let exps = filter(&table_vars, exps, *cond, env)?;
+            Ok((Table(table_vars, exps), env.clone()))
         }
         Union(l, r) => {
-            let (Table(vars, mut exps), _) = eval_exp(*l, env.clone())? else {
+            let (Table(vars, mut exps), _) = eval_exp(*l, env)? else {
                 return Err("expected table".to_string());
             };
-            let (Table(r_vars, mut r_exps), _) = eval_exp(*r, env.clone())? else {
+            let (Table(r_vars, mut r_exps), _) = eval_exp(*r, env)? else {
                 return Err("expected table".to_string());
             };
             if vars != r_vars {
                 return Err("expected tables with matching columns in union".to_string());
             }
             exps.append(&mut r_exps);
-            Ok((Table(vars, exps), env))
+            Ok((Table(vars, exps), env.clone()))
         }
         Difference(l, r) => {
-            let (Table(l_vars, l_exps), _) = eval_exp(*l, env.clone())? else {
+            let (Table(l_vars, l_exps), _) = eval_exp(*l, env)? else {
                 return Err("expected table".to_string());
             };
-            let (Table(r_vars, r_exps), _) = eval_exp(*r, env.clone())? else {
+            let (Table(r_vars, r_exps), _) = eval_exp(*r, env)? else {
                 return Err("expected table".to_string());
             };
             if l_vars != r_vars {
@@ -58,13 +59,13 @@ fn eval_exp(exp: Exp, mut env: Env) -> Result<(Exp, Env), String> {
                 .filter(|&l_exp| r_exps.chunks(vars.len()).all(|r_exp| l_exp != r_exp))
                 .flat_map(|chunk| chunk.to_vec())
                 .collect();
-            Ok((Table(vars, exps), env))
+            Ok((Table(vars, exps), env.clone()))
         }
         Product(l, r) => {
-            let (Table(l_vars, l_exps), _) = eval_exp(*l, env.clone())? else {
+            let (Table(l_vars, l_exps), _) = eval_exp(*l, env)? else {
                 return Err("expected table".to_string());
             };
-            let (Table(r_vars, r_exps), _) = eval_exp(*r, env.clone())? else {
+            let (Table(r_vars, r_exps), _) = eval_exp(*r, env)? else {
                 return Err("expected table".to_string());
             };
             let exps = l_exps
@@ -76,50 +77,50 @@ fn eval_exp(exp: Exp, mut env: Env) -> Result<(Exp, Env), String> {
                 })
                 .collect::<Vec<_>>();
             let vars = [l_vars, r_vars].concat();
-            Ok((Table(vars, exps), env))
+            Ok((Table(vars, exps), env.clone()))
         }
         Table(l, r) => {
             let exps = r
                 .into_iter()
-                .map(|exp| eval_exp(exp, env.clone()).map(|(exp, _)| exp))
+                .map(|exp| eval_exp(exp, env).map(|(exp, _)| exp))
                 .collect::<Result<Vec<Exp>, String>>()?;
-            Ok((Table(l, exps), env))
+            Ok((Table(l, exps), env.clone()))
         }
         Or(l, r) => {
-            if let (Bool(true), _) = eval_exp(*l, env.clone())? {
-                return Ok((Bool(true), env));
+            if let (Bool(true), _) = eval_exp(*l, env)? {
+                return Ok((Bool(true), env.clone()));
             }
-            if let (Bool(true), _) = eval_exp(*r, env.clone())? {
-                return Ok((Bool(true), env));
+            if let (Bool(true), _) = eval_exp(*r, env)? {
+                return Ok((Bool(true), env.clone()));
             }
-            Ok((Bool(false), env))
+            Ok((Bool(false), env.clone()))
         }
         Equals(l, r) => {
-            let (l, _) = eval_exp(*l, env.clone())?;
-            let (r, _) = eval_exp(*r, env.clone())?;
-            Ok((Bool(l == r), env))
+            let (l, _) = eval_exp(*l, env)?;
+            let (r, _) = eval_exp(*r, env)?;
+            Ok((Bool(l == r), env.clone()))
         }
         And(l, r) => {
-            if let (Bool(false), _) = eval_exp(*l, env.clone())? {
-                return Ok((Bool(false), env));
+            if let (Bool(false), _) = eval_exp(*l, env)? {
+                return Ok((Bool(false), env.clone()));
             }
-            if let (Bool(false), _) = eval_exp(*r, env.clone())? {
-                return Ok((Bool(false), env));
+            if let (Bool(false), _) = eval_exp(*r, env)? {
+                return Ok((Bool(false), env.clone()));
             }
-            Ok((Bool(true), env))
+            Ok((Bool(true), env.clone()))
         }
         Not(exp) => {
-            let exp = eval_exp(*exp, env.clone())?;
+            let exp = eval_exp(*exp, env)?;
             match exp {
-                (Bool(bool), _) => Ok((Bool(!bool), env)),
+                (Bool(bool), _) => Ok((Bool(!bool), env.clone())),
                 _ => Err(format!("Expected boolean, found {:?}", exp)),
             }
         }
         Var(var) => match env.get(&var) {
-            Some(exp) => Ok((exp.clone(), env)),
+            Some(exp) => Ok((exp.clone(), env.clone())),
             None => Err(format!("Variable `{}` not defined", var)),
         },
-        exp => Ok((exp, env)),
+        exp => Ok((exp, env.clone())),
     }
 }
 
@@ -142,12 +143,7 @@ fn select(keep: &[String], target: &[String], items: Vec<Exp>) -> Vec<Exp> {
         .collect()
 }
 
-fn filter(
-    vars: &[String],
-    exps: Vec<Exp>,
-    cond: Exp,
-    env: HashMap<String, Exp>,
-) -> Result<Vec<Exp>, String> {
+fn filter(vars: &[String], exps: Vec<Exp>, cond: Exp, env: &Env) -> Result<Vec<Exp>, String> {
     exps.chunks(vars.len())
         .try_fold(vec![], |mut acc: Vec<Exp>, exps: &[Exp]| {
             let mut env = env.clone();
@@ -155,7 +151,7 @@ fn filter(
                 env.insert(var.clone(), exp.clone());
             }
 
-            let (Bool(keep), _) = eval_exp(cond.clone(), env)? else {
+            let (Bool(keep), _) = eval_exp(cond.clone(), &env)? else {
                 return Err("expected boolean in where clause".to_string());
             };
 
